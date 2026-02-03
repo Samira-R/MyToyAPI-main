ba0ce55aea92bf5ec2c1c30408e67c4d4bf00ee5
Samira <samirarahmani020@gmail.com>
2026-02-04 01:15:22 +0330

    Add health endpoint and enable Swagger

--- Program.cs
+++ Program.cs
@@ -3,8 +3,23 @@
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+//-----------------------------
+//برای سویگر
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//--------------
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -44,6 +59,7 @@ app.MapGet("/health", () =>
 });
 
 
+//---------------------------
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
